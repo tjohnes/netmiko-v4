9e9eaebbe155ed0324cf0b23eadd5c544c9e5293
shreyash <shreyash@cisco.com>
2023-03-22 15:27:44 -0700

    to check if generic session_preparation works eith telnet handle

--- netmiko/cisco/cisco_xr.py
+++ netmiko/cisco/cisco_xr.py
@@ -237,12 +237,14 @@ class CiscoXrSSH(CiscoXrBase):
 
 class CiscoXrTelnet(CiscoXrBase):
     """Cisco XR Telnet driver."""
-
-    
+    pass
+    '''
     def session_preparation(self):
         """Prepare the session after the connection has been established."""
         self.set_base_prompt()
-    
+        self.disable_paging()
+        self.set_terminal_width(command='terminal width 511')
+    '''
 
 class CiscoXrFileTransfer(CiscoFileTransfer):
     """Cisco IOS-XR SCP File Transfer driver."""
