4d196ce2e18afefd29051bd64316b7fe7b82c8fa
shreyash <shreyash@cisco.com>
2023-03-22 15:36:51 -0700

    write newline before calling super().session_preparartion in CiscoXRTelnet

--- netmiko/cisco/cisco_xr.py
+++ netmiko/cisco/cisco_xr.py
@@ -238,13 +238,15 @@ class CiscoXrSSH(CiscoXrBase):
 class CiscoXrTelnet(CiscoXrBase):
     """Cisco XR Telnet driver."""
     pass
-    '''
+    
     def session_preparation(self):
         """Prepare the session after the connection has been established."""
-        self.set_base_prompt()
-        self.disable_paging()
-        self.set_terminal_width(command='terminal width 511')
-    '''
+        self.write_channel('\r\n')
+        super().session_preparation()
+        #self.set_base_prompt()
+        #self.disable_paging()
+        #self.set_terminal_width(command='terminal width 511')
+    
 
 class CiscoXrFileTransfer(CiscoFileTransfer):
     """Cisco IOS-XR SCP File Transfer driver."""
